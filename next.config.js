dcacb53ddd7ceac145db077a4978876337ea6628
Elis Bresciani <dddbresciani@gmail.com>
2022-10-06 17:00:53 -0300

    change location

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = () => {
     return [
       {
         source: "/",
-        destination: "http://localhost:5000/",
+        destination: "https://login-app-ponto.herokuapp.com/",
       },
     ];
   };
